3c80e9a0e5f8720921afafbc03a96850bd8ee715
VIKAS_PATEL <166948664+VikasPatel2004@users.noreply.github.com>
2025-07-22 19:11:07 +0530

    Array min max

---
+++ Data_Structures/Array/Array1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <limits.h>
+using namespace std ;
+
+int main() {
+    int size = 5;
+    int marks[size];
+    int smallest = INT_MAX;
+    
+
+    for (int i=0;i<size;i++){
+        cout << "Enter the marks : ";
+        cin>>marks[i];
+    }
+    
+    for(int i=0;i<size;i++){
+      // if (marks[i]<smallest){  //similarly we can do for max
+      //   smallest = marks[i];
+      // }
+      smallest = min(marks[i],smallest); //since in C++ min and max are implecit func
+    }
+    cout << smallest;
+    return 0;
+}
