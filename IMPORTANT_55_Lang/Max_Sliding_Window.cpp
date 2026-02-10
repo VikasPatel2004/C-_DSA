71485ade9a8dc91a94e40101b30956f319029df9
VIKAS_PATEL <166948664+VikasPatel2004@users.noreply.github.com>
2026-02-11 00:22:34 +0530

    max sliding window

--- IMPORTANT_55_Lang/Max_Sliding_Window.cpp
+++ IMPORTANT_55_Lang/Max_Sliding_Window.cpp
@@ -1,52 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm> // for max_element
+
 using namespace std;
 
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // stores indices of useful elements
-        vector<int> result;
-
-        for (int i = 0; i < nums.size(); i++) {
-            // Remove elements out of the current window
-            if (!dq.empty() && dq.front() == i - k) {
-                dq.pop_front();
-            }
-
-            // Remove smaller elements (they’ll never be needed)
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-
-            dq.push_back(i);
-
-            // Add max element of current window to result
-            if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
-            }
-        }
-
-        return result;
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> result;
+    for (int i = 0; i <= nums.size() - k; i++) {
+        // get max in current window
+        int windowMax = *max_element(nums.begin() + i, nums.begin() + i + k);
+        result.push_back(windowMax);
     }
-};
+    return result;
+}
 
 int main() {
-    Solution sol;
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    vector<int> ans = maxSlidingWindow(nums, k);
 
-    // Example 1
-    vector<int> nums1 = {1,3,-1,-3,5,3,6,7};
-    int k1 = 3;
-    vector<int> res1 = sol.maxSlidingWindow(nums1, k1);
-    cout << "Output for Example 1: ";
-    for (int x : res1) cout << x << " ";
-    cout << endl;
-
-    // Example 2
-    vector<int> nums2 = {1};
-    int k2 = 1;
-    vector<int> res2 = sol.maxSlidingWindow(nums2, k2);
-    cout << "Output for Example 2: ";
-    for (int x : res2) cout << x << " ";
+    for (int x : ans) {
+        cout << x << " ";
+    }
     cout << endl;
 
     return 0;
