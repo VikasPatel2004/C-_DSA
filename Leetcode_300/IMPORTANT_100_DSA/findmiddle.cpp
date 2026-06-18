8f66b428d62939b1c02603a94dea7fa6d370e139
VIKAS_PATEL <166948664+VikasPatel2004@users.noreply.github.com>
2026-06-19 00:05:04 +0530

    findmiddle in ll

---
+++ Leetcode_300/IMPORTANT_100_DSA/findmiddle.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow; 
+    }
+};
+
+// Helper function to create linked list from vector
+ListNode* createList(const vector<int>& vals) {
+    if (vals.empty()) return nullptr;
+    ListNode* head = new ListNode(vals[0]);
+    ListNode* current = head;
+    for (int i = 1; i < vals.size(); i++) {
+        current->next = new ListNode(vals[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+// Helper function to print linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Example test case
+    vector<int> vals = {1, 2, 3, 4, 5};
+    ListNode* head = createList(vals);
+
+    Solution sol;
+    ListNode* middle = sol.middleNode(head);
+
+    cout << "Linked List: ";
+    printList(head);
+    cout << "Middle Node Value: " << middle->val << endl;
+
+    return 0;
+}
